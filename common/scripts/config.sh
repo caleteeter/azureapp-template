d70830dd2b5a3366f6869459c45071e6ddee0f38
Cale Teeter <cale.teeter@microsoft.com>
2020-03-16 01:48:53 -0400

    Initial commit

---
+++ common/scripts/config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#############
+# Parameters
+#############
+AZURE_USER=$1
+ARTIFACTS_URL=$2
+ARTIFACTS_URL_SASTOKEN=$3
+DNS_NAME=$4
+
+#############
+# Constants
+#############
+HOME_DIR="/home/$AZURE_USER"
+
+##########################
+# Install OpenEnclave SDK
+##########################
+
+echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
+
+echo "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-7 main" | sudo tee /etc/apt/sources.list.d/llvm-toolchain-bionic-7.list
+wget -qO - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic main" | sudo tee /etc/apt/sources.list.d/msprod.list
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt -y install dkms
+wget https://download.01.org/intel-sgx/sgx-dcap/1.4/linux/distro/ubuntuServer18.04/sgx_linux_x64_driver_1.21.bin -O sgx_linux_x64_driver.bin
+chmod +x sgx_linux_x64_driver.bin
+sudo ./sgx_linux_x64_driver.bin
+
+sudo apt -y install clang-7 libssl-dev gdb libsgx-enclave-common libsgx-enclave-common-dev libprotobuf10 libsgx-dcap-ql libsgx-dcap-ql-dev az-dcap-client open-enclave
